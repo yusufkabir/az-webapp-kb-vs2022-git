fbad71776d5f7c0622492413cacfa21e3320fc18
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2023-06-14 12:51:06 +0100

    Added greeting to appsetting.json

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,14 +7,17 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewData["Greeting"] = _configuration["Greeting"];
             return View();
         }
 
